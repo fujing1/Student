ec62e13ede20a69dd67880acd87cd801866941ef
fujing1 <1422722503@qq.com>
2020-11-10 16:30:20 +0800

    第一次提交

---
+++ ModuleA/student.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define MAXN 50    //学生姓名，学好，课程名最大长度
+
+struct student //学生信息结构体
+{
+	char name[MAXN];      //姓名
+ 	char num[MAXN];  //学号
+	double score_sel = 0; //已选课总学分
+	char course[50][MAXN];  //记录选课课程的编号
+	struct student *next;  //链表指针
+};
---
+++ ModuleA/student.h
@@ -0,0 +1,1 @@
+#define MAXN 50
