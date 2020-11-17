624ada4c13001b9b1cef0985166b4312cb3f1186
fujing1 <1422722503@qq.com>
2020-11-17 16:58:37 +0800

    学生管理分支

--- ModuleA/student.h
+++ ModuleA/student.h
@@ -1,5 +1,5 @@
-#define MAXN 50    //学生姓名，学好，课程名最大长度
-#define STU_F  "/home/fujing/Student/ModuleA/student.txt"  //文件存放路径及文件名
+#define MAXN 50    //学生姓名，学号，课程名最大长度
+#define STU_F  "student.txt"  //文件存放路径及文件名
 
 struct student //学生信息结构体
 {
@@ -11,10 +11,15 @@ struct student //学生信息结构体
 	struct student *next;
 };
 
-struct student* load_stu();
-void menu();                                 //主菜单
+void load_stu();
+struct student *load_end(struct student *head);    //文件尾
+struct student *find_num(struct student *head,int num);  //根据学号查询
+struct student *find_name(struct student *head,char name[]);  //根据姓名查询
+void s_menu();                                 //主菜单
 void s_insert(struct student *head);        //增加学生的函数
 void s_show_num(struct student *head);       //根据学号展示学生信息
+void s_show_name(struct student *head);       //根据姓名展示学生信息
+void s_show(struct student *head);       //展示所有学生信息
 void s_delete(struct student *head);        //删除学生的函数
-void s_earch_stu(struct student *head);     //搜索学生的函数
-void s_modify(struct student *head);       //管理员修改学生信息
+void s_modify_num(struct student *head);       //根据学号修改学生信息
+void s_modify_name(struct student *head);       //根据姓名修改学生信息
